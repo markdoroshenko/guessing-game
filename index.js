a4882675330b564bed1a9dc816cd21cad9426a54
MarkDoroshenko <msv0804@gmail.com>
2023-09-29 10:23:45 +0300

    feat: add js code

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const GuessingGame = require('./src/guessing-game.js');
+const GuessingGame = require( './src/guessing-game.js' );
+
 
 window.game = new GuessingGame();
+
+//
+//
+// const game = new GuessingGame();
+// game.setRange(1, 100);
+// console.log('this.numToGuess', game.numToGuess);
+// const result = game.guess(33);
+// console.log(result);
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
+
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.numToGuess = 0;
     }
 
-    guess() {
-
+    setRange( min, max ) {
+        if ( min >= max ) {
+            throw new Error( "Invalid range: min must be less than max" );
+        }
+        this.min = min;
+        this.max = max;
     }
 
     lower() {
-
+        this.max = this.numToGuess
     }
 
     greater() {
+        this.min = this.numToGuess
+    }
 
+    guess() {
+        const arr = []
+        for (let i = this.min; i <= this.max; i++) {
+            arr.push(i);
+        }
+        this.numToGuess = arr[Math.floor(arr.length / 2)];
+
+        return this.numToGuess;
     }
 }
 
